aa41cf7e6f1c47bc93cf0a88291ba69b04d3d94f
jegasuguna <35062889+jegasuguna@users.noreply.github.com>
2018-04-20 15:27:33 +0530

    Update Jobname.ps1

--- Jobname.ps1
+++ Jobname.ps1
@@ -1,17 +1,17 @@
-﻿#Declare the file path and sheet name
+
 $file = "D:\Suguna\Book2.xlsx"
 $sheetName = "Sheet1"
-#Create an instance of Excel.Application and Open Excel file
+
 $objExcel = New-Object -ComObject Excel.Application
 $workbook = $objExcel.Workbooks.Open($file)
 $sheet = $workbook.Worksheets.Item($sheetName)
 $objExcel.Visible=$false
-#Count max row
+
 $rowMax = ($sheet.UsedRange.Rows).count
-#Declare the starting positions
+
 $rowName,$colName = 1,1
 $rowParameters,$colParameters = 1,2
-#loop to get values and store it
+
 for ($i=1; $i -le $rowMax-1; $i++)
 {
 $name = $sheet.Cells.Item($rowName+$i,$colName).text
@@ -20,5 +20,5 @@ $parameters = $sheet.Cells.Item($rowParameters+$i,$colParameters).text
 Write-Host ($name+"   "+$parameters)
 
 }
-#close excel file
+
 $objExcel.quit()
